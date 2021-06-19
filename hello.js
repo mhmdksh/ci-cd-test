3eb2aa6060621eff42331cd5e52860e076a20cbc
mhmdksh <eddsnx3@gmail.com>
2021-06-19 18:29:51 +0000

    Initial Commit

---
+++ hello.js
@@ -0,0 +1,8 @@
+/* Hello, World! program in node.js */
+/*console.log("Hello, World!")*/
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Travis!\n')
+}).listen(1337, 'mhmdksh.com');
+console.log('Server running at http://mhmdksh.com:1337/');
